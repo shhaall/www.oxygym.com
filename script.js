70e8f0f716437df19e0624dcd8a705b404e04909
shhaall <mohammedshahal244@gmail.com>
2024-08-28 01:07:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function navToggle(){
   togglebar.classList.toggle("active");
   menu.classList.toggle("active");
 };
+
+
+
+
+const scriptURL ='https://script.google.com/macros/s/AKfycbyiSHgiJMpt9_e8m8stSPT8pM3CYBDfjVgt3dIYtKFiNKGgFOOjvE10PH_Cygfh8tJd/exec'
+
+const form =  document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(Response => alert("Thank you! your form is submitted successfully."))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+  
+
+
+})
